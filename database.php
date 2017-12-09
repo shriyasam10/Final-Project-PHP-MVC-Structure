8a7e875dcc31032d1bb6482f563c985eb415441e
Shriya S Samak <sss329@afsaccess1.njit.edu>
2017-12-08 19:26:23 -0500

    added databse credentials

--- database.php
+++ database.php
@@ -1,6 +1,6 @@
 <?php
 
-define('DATABASE', 'kwilliam');
-define('USERNAME', 'kwilliam');
-define('PASSWORD', 'USracpEto');
+define('DATABASE', 'sss329');
+define('USERNAME', 'sss329');
+define('PASSWORD', 'Iag2xyAqh');
 define('CONNECTION', 'sql2.njit.edu');
