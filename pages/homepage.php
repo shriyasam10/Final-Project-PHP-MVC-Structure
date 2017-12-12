6a687e843c502fd19df619cfaea8107eaff147ab
Shriya S Samak <sss329@afsaccess1.njit.edu>
2017-12-12 18:43:19 -0500

    Some changes in login form

--- pages/homepage.php
+++ pages/homepage.php
@@ -32,11 +32,11 @@
 <form action="index.php?page=accounts&action=login" method="POST">
 
     <div class="container">
-        <label><b>Username</b></label>
-        <input type="text" placeholder="Enter Username" name="email" required>
+        <label><b>Username:</b></label><br>
+        <input type="text" placeholder="Enter Username" name="email" required><br><br>
 
-        <label><b>Password</b></label>
-        <input type="password" placeholder="Enter Password" name="password" required>
+        <label><b>Password</b></label><br>
+        <input type="password" placeholder="Enter Password" name="password" required><br><br>
 
         <button type="submit">Login</button>
     </div>
