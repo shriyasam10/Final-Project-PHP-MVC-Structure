bef1cbe98b9d7225f0c921d3e6add699a96b134a
Shriya S Samak <sss329@afsconnect2.njit.edu>
2017-12-17 21:27:42 -0500

    Some changes

--- pages/show_task.php
+++ pages/show_task.php
@@ -1,38 +1,3 @@
-/*<!--<!doctype html>>
-
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-
-    <title>The HTML5 Herald</title>
-    <meta name="description" content="The HTML5 Herald">
-    <meta name="author" content="SitePoint">
-
-    <link rel="stylesheet" href="css/styles.css?v=1.0">
-
-    <!--[if lt IE 9]>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
-    <![endif]-->
-</head>
-
-<body>
-
-<?php
-//this is how you print something  $data contains the record that was selected on the table.
-
-print_r($data);
-?>
-<form action="index.php?page=tasks&action=delete&id=<?php echo $data->id; ?> " method="post" id="form1">
-    <button type="submit" form="form1" value="delete">Delete</button>
-</form>
-
-
-
-
-<script src="js/scripts.js"></script>
-</body>
-<!</html>--> */
-
 <!doctype html>
 
 <html lang="en">
@@ -56,7 +21,7 @@ print_r($data);
 //this is how you print something  $data contains the record that was selected on the table.
 print_r($data);
 ?>
-<h6><a href="index.php?page=accounts&action=logout">Logout</a></h6>
+<h3><a href="index.php?page=accounts&action=logout">Logout</a></h3>
 <form action="index.php?page=tasks&action=edit&id=<?php echo $data->id; ?>" method="get">
 
     Todo Title/body: <?php echo $data->message; ?><br>
@@ -76,39 +41,3 @@ print_r($data);
 <script src="js/scripts.js"></script>
 </body>
 </html>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
